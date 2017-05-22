87ae18f09fcf68fb58299474202b6711d9df946f
Jess Melbourne-Thomas <Jess.Melbourne-Thomas@aad.gov.au>
2017-05-22 13:23:24 +1000

    Add files via upload

---
+++ SOsolardummy.ts
@@ -0,0 +1,15 @@
+
+## COLUMNS 2
+##
+## COLUMN1.name Time
+## COLUMN1.long_name Time
+## COLUMN1.units days since 1990-01-01 00:00:00 +10
+## COLUMN1.missing_value -999.000000
+##
+## COLUMN2.name swr
+## COLUMN2.long_name Short wave radiation
+## COLUMN2.units W m-2
+## COLUMN2.missing_value -999.000000
+##
+
+1 100
